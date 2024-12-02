d6ed624f998ae14270139ea98f1707887e11ca98
Yash <yjdobariya0@gmail.com>
2024-12-02 23:26:38 +0530

    added top loadin bar

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -18,8 +18,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <p className='card-text'><small className='text-muted'>Source: {source}</small><br></br>
-                            <small className='text-muted'>By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                        <p className='card-text'><small className='text-muted'>By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary btn-dark">Read more</a>
                     </div>
                 </div>
