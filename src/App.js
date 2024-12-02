10942fbf868ab46a81387ccdba22b876d5f3e27f
Yash <yjdobariya0@gmail.com>
2024-12-02 22:57:35 +0530

    added infinite loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<News key="general" pageSize={15} country="us" category="general" />} />
+            <Route path="/business" element={<News key="business" pageSize={15} country="us" category="business" />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={15} country="us" category="entertainment" />} />
+            <Route path="/general" element={<News key="general" pageSize={15} country="us" category="general" />} />
+            <Route path="/health" element={<News key="health" pageSize={15} country="us" category="health" />} />
+            <Route path="/science" element={<News key="science" pageSize={15} country="us" category="science" />} />
+            <Route path="/sports" element={<News key="sports" pageSize={15} country="us" category="sports" />} />
+            <Route path="/technology" element={<News key="technology" pageSize={15} country="us" category="technology" />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/components/News.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  constructor(props) {
+    super();
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+      totalResults: 0
+    }
+  }
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71dc23a23abc4a249c6d4d763f7addd6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    })
+  }
+
+  async componentDidMount() {
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsApp`;
+    this.updateNews();
+  }
+
+  // handlePreviousClick = async () => {
+  //   this.setState({ page: this.state.page - 1 });
+  //   this.updateNews();
+  // }
+  // handleNextClick = async () => {
+  //   this.setState({ page: this.state.page + 1 });
+  //   this.updateNews();
+  // }
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=71dc23a23abc4a249c6d4d763f7addd6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    })
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className='text-center my-3 p-2 text-center bg-warning rounded-pill' style={{ margin: '35px 0px' }}>Today's Top <span className="fw-bold text-primary">{this.capitalizeFirstLetter(this.props.category)}</span> Headlines</h1>
+        {this.state.loading && <Spinner/>} 
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Spinner />}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen all the news!</b>
+            </p>
+          }
+        >
+          <div className='container my-3'>
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return <div className="col-md-4" key={element.url}>
+                  <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                </div>
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
+          <button disabled={this.state.page + 1 >= Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div> */}
+      </>
+    )
+  }
+}
+
+export default News
+
+// // liseki1098@kindomd.com
+// // 71dc23a23abc4a249c6d4d763f7addd6
